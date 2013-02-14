10bcd9b92251a04b3a77ac0d5800de2d5dc00d5a
sbeltz <shelley.beltz@gettyimages.com>
2013-02-14 10:32:06 -0700

    finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+    factory :user do
+        name        "Emilia Earhart"
+        email       "fake@nowhere.com"
+        password    "foobar"
+        password_confirmation   "foobar"
+    end
+
+end
